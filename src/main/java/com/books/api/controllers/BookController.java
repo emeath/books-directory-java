4eb5db5d089ea85f65a06f961c6b55b20a5b487e
emeath <matheus.martins.jeronimo@gmail.com>
2022-07-29 10:19:20 -0300

    Get all books pageable

--- src/main/java/com/books/api/controllers/BookController.java
+++ src/main/java/com/books/api/controllers/BookController.java
@@ -1,12 +1,15 @@
 package com.books.api.controllers;
 
-import java.util.List;
 import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -32,8 +35,8 @@ public class BookController {
 	BookService bookService;
 	
 	@GetMapping
-	public ResponseEntity<List<BookModel>> getAllBooks(){
-		return ResponseEntity.status(HttpStatus.OK).body(bookService.findAll());
+	public ResponseEntity<Page<BookModel>> getAllBooks(@PageableDefault(page = 0, size = 3, sort = "name", direction = Sort.Direction.ASC) Pageable pageable){
+		return ResponseEntity.status(HttpStatus.OK).body(bookService.findAll(pageable));
 	}
 	
 	@GetMapping("/{id}")
--- src/main/java/com/books/api/services/BookService.java
+++ src/main/java/com/books/api/services/BookService.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.books.api.models.BookModel;
@@ -38,6 +40,10 @@ public class BookService {
 	public void delete(Long id) {
 		bookRepository.deleteById(id);
 		
+	}
+
+	public Page<BookModel> findAll(Pageable pageable) {
+		return bookRepository.findAll(pageable);
 	}
 
 }
