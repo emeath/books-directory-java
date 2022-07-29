3003f7c4d8cd79dc5a0e401defb6929ddd796758
emeath <matheus.martins.jeronimo@gmail.com>
2022-07-29 14:56:15 -0300

    Updates import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,17 @@
-INSERT INTO TB_BOOKS(id, name, author_Name, pages, genre) VALUES (10, 'Klara and the Sun', 'Kazuo Ishiguro', 341, 'Ficção Científica');
-INSERT INTO TB_BOOKS(id, name, author_Name, pages, genre) VALUES (20, 'Detransition, Baby', 'Torrey Peters', 320, 'Ficção Domestica');
-INSERT INTO TB_BOOKS(id, name, author_Name, pages, genre) VALUES (30, 'Verity', 'Colleen Hoover', 336, 'Suspense');
+INSERT INTO TB_BOOKS(id, name, author_Name, pages, genre) VALUES (42,'Antes que o café esfrie','Toshikazu Kawaguchi',208,'Literatura e Ficção');
+INSERT INTO TB_BOOKS(id, name, author_Name, pages, genre) VALUES (43,'As coisas que você só vê quando desacelera','Haemin Sunim',256,'Espiritual  Autoajuda');
+INSERT INTO TB_BOOKS(id, name, author_Name, pages, genre) VALUES (44,'O tatuador de Auschwitz','Heather Morris',240,'Romance Histórico Romance');
+INSERT INTO TB_BOOKS(id, name, author_Name, pages, genre) VALUES (45,'O sol é para todos','Harper Lee',349,'Clássicos de Ficção');
+INSERT INTO TB_BOOKS(id, name, author_Name, pages, genre) VALUES (46,'A coragem de ser imperfeito','Brené Brown',208,'Emoções Saúde, Boa Forma e Dieta');
+INSERT INTO TB_BOOKS(id, name, author_Name, pages, genre) VALUES (47,'A paciente silenciosa','Alex Michaelides',364,'Livros de Suspenses de Espionagem e Política');
+INSERT INTO TB_BOOKS(id, name, author_Name, pages, genre) VALUES (48,'Diário de uma ansiosa ou como parei de me sabotar','Beth Evans',192,'Ficção Literária Literatura e Ficção');
+INSERT INTO TB_BOOKS(id, name, author_Name, pages, genre) VALUES (49,'A casa das lembranças perdidas',' Kate Morton',480,'Livros de Romance Histórico do Século 20');
+INSERT INTO TB_BOOKS(id, name, author_Name, pages, genre) VALUES (50,'Em Algum Lugar nas Estrelas','Clare Vanderpool',288,'Livros de Fantasia e Terror para Adolescentes');
+INSERT INTO TB_BOOKS(id, name, author_Name, pages, genre) VALUES (51,'Pra você que sente demais','Victor Fernandes',176,'Ficção Literária Literatura e Ficção');
+INSERT INTO TB_BOOKS(id, name, author_Name, pages, genre) VALUES (52,'É Assim que Acaba','Colleen Hoover',368,'Livros de Romance para Jovens');
+INSERT INTO TB_BOOKS(id, name, author_Name, pages, genre) VALUES (53,'Um Lugar Bem Longe Daqui','Delia Owens',336,'Drama Literatura e Ficção');
+INSERT INTO TB_BOOKS(id, name, author_Name, pages, genre) VALUES (54,'Quando Ninguém Está Olhando','Alyssa Cole',400,'Psicológicos Mistério, Thriller e Suspense');
+INSERT INTO TB_BOOKS(id, name, author_Name, pages, genre) VALUES (55,'Em busca de nós mesmos','Clóvis de Barros Filho',400,'Paternidade Paternidade e Relacionamentos');
+INSERT INTO TB_BOOKS(id, name, author_Name, pages, genre) VALUES (56,'Mentes brilhantes: o seu cérebro','Alberto Dell-isola',128,'Melhoria da Memória');
+INSERT INTO TB_BOOKS(id, name, author_Name, pages, genre) VALUES (57,'Mente afiada: cérebro ativo e saudável','Sanjay Gupta',320,'Transtornos e Doenças Saúde e Família');
 
